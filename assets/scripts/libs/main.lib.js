a4c2e972e31f472ff150dde12baf0465b5a27517
LateDream <127290936+latedreamdev@users.noreply.github.com>
2024-12-13 16:25:44 +0800

    ✨ feat: add metadata support

--- assets/scripts/libs/main.lib.js
+++ assets/scripts/libs/main.lib.js
@@ -1,22 +1,5 @@
 window.result = document.querySelector("#result");
-
-export const mcpackM = {
-	Manifest: function(name, description, version, minBedrockVersion, type) {
-		let mBv = minBedrockVersion.split('.');
-		this.format_version = (mBv[0] == 1 && mBv[1] >= 13)? 2: 1;
-		this.header = {
-			description: description,
-			name: name,
-			version: version.split('.').map(Number),
-			min_engine_version: minBedrockVersion.split('.').map(Number)
-		};
-		this.modules = [{
-			type: type,
-			version: version.split('.').map(Number)
-		}]
-		this.header.uuid = this.modules[0].uuid = mcpackM.generateUUID();
-	},
-	generateUUID() {
+const generateUUID = () => {
 		let d = new Date().getTime();
 		let uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
 			let r = (d + Math.random() * 16) % 16 | 0;
@@ -24,26 +7,61 @@ export const mcpackM = {
 			return (c == 'x'? r: (r & 0x3 | 0x8)).toString(16);
 		});
 		return uuid;
-	},
+	}
+
+/**
+ * Manifest class
+ * @param {string} name - pack name
+ * @param {string} description - pack description
+ * @param {string} version - pack version
+ * @param {string} minBedrockVersion - minimum bedrock version
+ * @param {string} type - pack type
+ * @param {string} uuid - uuid v4
+ */
+class Manifest {
+	constructor(name, description, version, minBedrockVersion, type, authors, license, url) {
+		this.format_version = (minBedrockVersion.split('.')[0] == 1 && minBedrockVersion.split('.')[1] >= 13)? 2: 1;
+		this.header = {
+			description, name, version: version.split('.').map(Number), min_engine_version: minBedrockVersion.split('.').map(Number),
+			uuid: generateUUID()
+		};
+		this.modules = [{
+			type, version: version.split('.').map(Number),
+			uuid: generateUUID()
+		}];
+		if(authors) this.metadata.authors = authors.split(',').map(s => s.trim()) || [];
+		if(license) this.metadata.license = license;
+		if(url) this.metadata.url = url;
+	}
+	metadata = {
+		generated_with: {
+			"latedream-mcpack-manifest-generator": [mcpackM.version.number.join('.')]
+		}
+	}
+}
+// debug
+globalThis.Manifest = Manifest;
+
+export const mcpackM = {
 	generation() {
 		const mcPackInfo = new FormData(document.querySelector("form#mcPackInfo"));
 		let mBv = mcPackInfo.get("minBedrockVersion");
 		if(mBv.split('.')[0] > 1) {
 			alert(`what? minecraft has been update to ${mBv.join('.')}???`);
 			return;
-		}
+		} else
 		if(mBv.split('.')[0] < 1 && mBv.split('.')[1] < 8) {
 			alert("min bedrock version is too low!!\nlowest: 1.8.0");
 			return;
 		}
 		result.toggleAttribute("isUUID");
-		result.value = (JSON.stringify(new this.Manifest(mcPackInfo.get("name"), mcPackInfo.get("description"), mcPackInfo.get("version"), mBv, mcPackInfo.get("type"))));
+		result.value = (JSON.stringify(new Manifest(mcPackInfo.get("name"), mcPackInfo.get("description"), mcPackInfo.get("version"), mBv, mcPackInfo.get("type"), mcPackInfo.get("authors"), mcPackInfo.get("license"), mcPackInfo.get("url"))));
 		document.querySelector("#buttonPanel").style.display = "inline";
 		window.alert("success!");
 	},
 	version: {
-		number: [0, 2, 4],
-		build: "20241116",
+		number: [0, 2, 6],
+		build: "20241213",
 		name: "beta"
 	},
 	getVer() {
